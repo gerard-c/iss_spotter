d39f4277537eddf633ac4b74e6bd67790987a027
Gerard Compion <gcompion93@gmail.com>
2022-01-20 17:01:47 -0500

    added responses to request error and/or bad status code

--- iss.js
+++ iss.js
@@ -10,10 +10,19 @@ const request = require('request');
  */
 const fetchMyIP = (callback) => {
   request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) callback(error, null);
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
     const data = JSON.parse(body);
     const myIP = data.ip;
     callback(null, myIP);
+
   });
 };
 
