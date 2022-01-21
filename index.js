86f4b8335106c691e2a132adb8badb68ca492de3
Gerard Compion <gcompion93@gmail.com>
2022-01-20 19:43:03 -0500

    final linting/comments

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
+// final function places information about ISS passes into a readable phrase
+// information about passes stored in array of objects, with each object being its own pass time
+const issPasses = (passes) => {
+  // iterates through array of objects
+  for (const pass of passes) {
+    // creates date object for each pass
+    const date = new Date(0);
+    // uses risetime key of pass object to set detailed information into date object
+    date.setUTCSeconds(pass.risetime);
+    // duration key of pass object stored in a variable
+    const duration = pass.duration;
+    // prints phrase with time/date/duration information from each pass
+    console.log(`Next pass at ${date} for ${duration} seconds!`);
+  }
+};
+
+// call of chain function that sets everything off ending in ISS passes array
 nextISSTimesForMyLocation((error, passes) => {
   if (error) {
     console.log(`Error in chaining: ${error}`);
   }
+  // calls final function using passes array provided by chain
   issPasses(passes);
 });
-
-const issPasses = (passes) => {
-  for (const pass of passes) {
-    const date = new Date(0);
-    date.setUTCSeconds(pass.risetime);
-    const duration = pass.duration;
-    console.log(`Next pass at ${date} for ${duration} seconds!`);
-  }
-};
--- iss.js
+++ iss.js
@@ -2,53 +2,64 @@ const request = require('request');
 
 
 const fetchMyIP = (callback) => {
+  // API that provides IP
   request('https://api.ipify.org?format=json', (error, response, body) => {
+    // Checks for error found by request
     if (error) {
       callback(error, null);
       return;
     }
+    // Checks for invalid status code
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching IP. Response ${body}`;
       callback(Error(msg), null);
       return;
     }
+    // IP passed into callback to be used by other functions
     callback(null, JSON.parse(body).ip);
   });
 };
 
 const fetchCoordsByIP = (ip, callback) => {
+  // API that provides coordinates via IP address
   request(`https://freegeoip.app/json/${ip}`, (error, response, body) => {
+    // Checks for error found by request
     if (error) {
       callback(error, null);
       return;
     }
+    // Checks for invalid status code
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching coords. Response ${body}`;
       callback(Error(msg), null);
       return;
     }
     const { latitude, longitude } = JSON.parse(body);
-
+    // coordinates passed into callback to be used by other functions
     callback(null, { latitude, longitude });
   });
 };
 
 const fetchISSFlyOverTimes = (coords, callback) => {
+  // API that provides info about visible orbit of ISS via coordinates
   request(`https://iss-pass.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    // Checks for error found by request
     if (error) {
       callback(error, null);
       return;
     }
+    // Checks for invalid status code
     if (response.statusCode !== 200) {
       const msg = `Status Code ${response.statusCode} when fetching flyover times. Response ${body}`;
       callback(Error(msg), null);
       return;
     }
-    const passes = JSON.parse(body).response;
-    callback(null, passes);
+    // array containing 5 objects with info about ISS orbit passed into callback for later use
+    callback(null, JSON.parse(body).response);
   });
 };
 
+// function that chains together previous functions, passing in results of previous callbacks and calling callbacks for next function in chain
 const nextISSTimesForMyLocation = (callback) => {
   fetchMyIP((error, ip) => {
     if (error) {
