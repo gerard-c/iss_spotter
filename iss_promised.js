366a28e62b2231ad71b1b41b0bee7910ff1a7c3e
Gerard Compion <gcompion93@gmail.com>
2022-01-20 20:16:22 -0500

    added and tested second function using promises

--- iss_promised.js
+++ iss_promised.js
@@ -4,4 +4,9 @@ const fetchMyIP = () => {
   return request('https://api.ipify.org?format=json');
 };
 
-module.exports = { fetchMyIP };
+const fetchCoordsByIP = (body) => {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+module.exports = { fetchMyIP, fetchCoordsByIP };
